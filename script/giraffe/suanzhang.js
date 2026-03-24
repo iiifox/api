08acfa1eaddfe9e0f1d082dd9db600fc1ede2591
iiifox <86429225+iiifox@users.noreply.github.com>
2026-03-24 20:41:01 +0800

    Update suanzhang.js

--- script/giraffe/suanzhang.js
+++ script/giraffe/suanzhang.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         长颈鹿导出(结算版)
 // @namespace    https://api.luei.me/
-// @version      1.0.0
+// @version      1.0.1
 // @description  长颈鹿内附Qb算账简易版
 // @author       luei
 // @match        *://115.29.174.100:8369/dltj.aspx*
@@ -28,6 +28,7 @@
         桃桃: {name: ["I-tao"]},
         抖音晚安: {name: ["I-wan"]},
         猫腻: {name: ["I-maoni"]},
+        匿名: {name: ["I-niming"]},
     };
 
     // ================== 数据缓存 ==================
